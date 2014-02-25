1d46f72f1b5329bdc6bce2d0c3c747b154a6dbcf
tylernorth <tylernorth18@gmail.com>
2014-02-25 12:36:16 -0800

    Changed from HTHMLParser to beautiful soup

--- download_scores.py
+++ download_scores.py
@@ -1,5 +1,5 @@
 import argparse
-from HTMLParser import HTMLParser
+from bs4 import BeautifulSoup
 import requests
 import os
 import sys
@@ -7,86 +7,33 @@ import sys
 MAIN_PREFIX = 'http://www.baseball-reference.com'
 BOXES = MAIN_PREFIX + '/boxes/'
 
-class list_teams(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.track_data = False
-        self.teams = dict()
-        self.last_team = None
-
-    def find_teams(self, data):
-        self.feed(data)
-        if '\n' in self.teams.keys():
-            del self.teams['\n']
-        return self.teams
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'div':
-            if len(attrs) > 0:
-                if attrs[0][0] == 'id':
-                    if attrs[0][1] == 'page_content':
-                        self.track_data = True
-        elif self.track_data:
-            if tag == 'a':
-                self.last_team = attrs[0][1]
-
-    def handle_endtag(self, tag):
-        if self.track_data:
-            if tag == 'table':
-                self.track_data = False
-
-    def handle_data(self, data):
-        if self.track_data and self.last_team is not None:
-            self.teams[data] = self.last_team
-
-class list_games(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.check_data = False
-        self.track_data = False
-        self.games = []
-    def list_games(self, data):
-        self.feed(data)
-        return self.games
-    def handle_starttag(self, tag, attrs):
-        if self.track_data:
-            if tag == 'a':
-                if attrs[0][1].startswith('/boxes/'):
-                    self.games.append(attrs[0][1])
-        if tag == 'h2':
-            self.check_data = True
-        else:
-            self.check_data = False
-    def handle_endtag(self, tag):
-        if tag == 'table':
-            self.track_data = False
-    def handle_data(self, data):
-        if self.check_data:
-            if data == 'Team Game-by-Game Schedule and Results':
-                self.track_data = True
-
-def get_abbreviation(link):
-    things = link.split('/')
-    return things[2]
-
-def get_team_info(year):
+def collect_teams(year):
     url = BOXES + year + '.shtml'
     r = requests.get(url)
-    check_results(r, 'Unable to retrieve data for the year requested.')
-    parser = list_teams()
-    teams = parser.find_teams(r.text)
+    check_results(r, msg='Unable to retrieve year:%s' % year)
+    soup = BeautifulSoup(r.text)
+    div = soup.find(id='page_content')
+    div_contents = div.contents[3].contents[0]
+    contents = div_contents.contents[0].contents[1]
+    teams = contents.find_all('a')
     team_data = dict()
-    for team, link in teams.iteritems():
-        abbr = get_abbreviation(link)
-        good_link = MAIN_PREFIX + link
-        team_data[abbr] = {'name' : team,
-                           'link' : good_link}
+    for team in teams:
+        team_name = team.contents[0]
+        team_url = team.attrs['href']
+        team_data[team_name] = team_url
     return team_data
 
-def get_team_games(team_link):
-    r = requests.get(team_link)
-    parser = list_games()
-    games = parser.list_games(r.text)
+def collect_games(link):
+    url = MAIN_PREFIX + link
+    r = requests.get(url)
+    check_results(r, msg='Unable to retrieve team:%s' % link)
+    soup = BeautifulSoup(r.text)
+    references = soup.find_all('a')
+    games = set([])
+    for ref in references:
+        href = ref.attrs['href']
+        if href.startswith('/boxes/') and href.endswith('.shtml'):
+            games.add(href)
     return games
 
 def parse_args():
@@ -112,13 +59,12 @@ def main():
         if not os.path.isdir(args['save_dir']):
             os.mkdir(args['save_dir'])
         output_dir = args['save_dir']
-    print 'Getting Team List'
-    teams = get_team_info(args['year'])
+
+    teams = collect_teams(args['year'])
     all_games = set([])
-    for abbr in teams:
-        print 'Collecting games for team:', teams[abbr]['name']
-        games = get_team_games(teams[abbr]['link'])
-        all_games = all_games.union(set(games))
+    for team, link in teams.iteritems():
+        print 'Collecting games for team:', team
+        all_games = all_games.union(collect_games(link))
     count = 0
     percent = 0.0
     total = len(all_games) * 1.0
@@ -131,7 +77,7 @@ def main():
         check_results(r, 'Unable to retrieve data for the game requested.')
         file_name = game.split('/')[3]
         with open(os.path.join(output_dir, file_name), 'w+') as f:
-            f.write(r.text)
+            f.write(r.text.encode('utf-8'))
         count += 1
 
 if __name__ == '__main__':
